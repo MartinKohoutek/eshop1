f28856cc05b235c2577a50c2a3f30f90b7790025
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-07-24 17:51:20 +0200

    Admin Profile 2

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -22,11 +22,11 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center text-center">
-                                <img src="{{ asset('backend/assets/images/avatars/avatar-2.png') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+                                <img src="{{ (!empty($admin->photo)) ? url('upload/admin_images/'.$admin->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
                                 <div class="mt-3">
-                                    <h4>John Doe</h4>
-                                    <p class="text-secondary mb-1">Full Stack Developer</p>
-                                    <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                                    <h4>{{ $admin->name }}</h4>
+                                    <p class="text-secondary mb-1">{{ $admin->email }}</p>
+                                    <p class="text-muted font-size-sm">{{ $admin->address }}</p>
                                 </div>
                             </div>
                             <hr class="my-4" />
@@ -54,10 +54,18 @@
                         <div class="card-body">
                             <div class="row mb-3">
                                 <div class="col-sm-3">
+                                    <h6 class="mb-0">User Name</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" class="form-control" value="{{ $admin->username }}" disabled />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
                                     <h6 class="mb-0">Full Name</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="John Doe" />
+                                    <input type="text" class="form-control" value="{{ $admin->name }}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -65,7 +73,7 @@
                                     <h6 class="mb-0">Email</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="john@example.com" />
+                                    <input type="email" class="form-control" value="{{ $admin->email }}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -73,15 +81,7 @@
                                     <h6 class="mb-0">Phone</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="(239) 816-9029" />
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-sm-3">
-                                    <h6 class="mb-0">Mobile</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="(320) 380-4539" />
+                                    <input type="text" class="form-control" value="{{ $admin->phone }}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -89,7 +89,23 @@
                                     <h6 class="mb-0">Address</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="Bay Area, San Francisco, CA" />
+                                    <input type="text" class="form-control" value="{{ $admin->address }}" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Photo</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="file" class="form-control" id="image" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0"></h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                <img src="{{ (!empty($admin->photo)) ? url('upload/admin_images/'.$admin->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="p-1 bg-primary" width="110">
                                 </div>
                             </div>
                             <div class="row">

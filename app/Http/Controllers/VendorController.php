14b130e5aa7d4ed4fa688282a58e4bb5daa4c9ee
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-07-25 16:14:39 +0200

    Vendor Login & Logout

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VendorController extends Controller
 {
     public function VendorDashboard() {
         return view('vendor.index');
+    }
+
+    public function VendorLogin() {
+        return view('vendor.vendor_login');
+    }
+
+    public function VendorLogout(Request $request) {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/vendor/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,9 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 
 Route::middleware(['auth', 'role:vendor'])->group(function(){
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+    Route::get('/vendor/logout', [VendorController::class, 'VendorLogout'])->name('vendor.logout');
     
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);

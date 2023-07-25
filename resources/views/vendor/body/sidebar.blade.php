731e258837f986760c481ff3ba2472541c87a98a
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-07-25 16:06:42 +0200

    Vendor Template Setup

---
+++ resources/views/vendor/body/sidebar.blade.php
@@ -0,0 +1,73 @@
+<!--sidebar wrapper -->
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="{{ asset('backend/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Vendor</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+    <li>
+            <a href="{{ route('vendor.dashboard') }}">
+                <div class="parent-icon"><i class='bx bx-home-alt'></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class='bx bx-home-alt'></i>
+                </div>
+                <div class="menu-title">Brand</div>
+            </a>
+            <ul>
+                <li> <a href="index.html"><i class='bx bx-radio-circle'></i>Default</a>
+                </li>
+                <li> <a href="index2.html"><i class='bx bx-radio-circle'></i>Alternate</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bx bx-category"></i>
+                </div>
+                <div class="menu-title">Application</div>
+            </a>
+            <ul>
+                <li> <a href="app-emailbox.html"><i class='bx bx-radio-circle'></i>Email</a>
+                </li>
+                <li> <a href="app-chat-box.html"><i class='bx bx-radio-circle'></i>Chat Box</a>
+                </li>
+            </ul>
+        </li>
+       
+        <li class="menu-label">Charts & Maps</li>
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bx bx-line-chart"></i>
+                </div>
+                <div class="menu-title">Charts</div>
+            </a>
+            <ul>
+                <li> <a href="charts-apex-chart.html"><i class='bx bx-radio-circle'></i>Apex</a>
+                </li>
+                <li> <a href="charts-chartjs.html"><i class='bx bx-radio-circle'></i>Chartjs</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="https://themeforest.net/user/codervent" target="_blank">
+                <div class="parent-icon"><i class="bx bx-support"></i>
+                </div>
+                <div class="menu-title">Support</div>
+            </a>
+        </li>
+    </ul>
+    <!--end navigation-->
+</div>
+<!--end sidebar wrapper -->

31bec7263a4cb89384fd7522622d9265520fbe1a
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-08-14 14:15:27 +0200

    Frontend - Recently Added & Special Deals Display

--- resources/views/frontend/home/home_bottom_products.blade.php
+++ resources/views/frontend/home/home_bottom_products.blade.php
@@ -2,6 +2,8 @@
 @php
 $hot_deals = App\Models\Product::where('hot_deals', 1)->where('discount_price', '!=', NULL)->orderBy('id', 'DESC')->limit(4)->get();
 $special_offer = App\Models\Product::where('special_offer', 1)->orderBy('id', 'DESC')->limit(4)->get();
+$recently_added = App\Models\Product::where('status', '1')->orderBy('id', 'DESC')->limit(4)->get();
+$special_deals = App\Models\Product::where('special_deals', '1')->orderBy('id', 'DESC')->limit(4)->get();
 @endphp
 <section class="py-4 border-top">
     <div class="container">
@@ -88,162 +90,81 @@ $special_offer = App\Models\Product::where('special_offer', 1)->orderBy('id', 'D
             </div>
             <div class="col">
                 <div class="new-arrivals-list mb-3">
-                    <h6 class="mb-3 text-uppercase">New arrivals</h6>
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="jproduct-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/09.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
+                    <h6 class="mb-3 text-uppercase">Recently Added</h6>
+                    @foreach ($recently_added as $key => $item)
                     <div class="d-flex align-items-center">
                         <div class="bottom-product-img">
                             <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/10.png') }}" width="100" alt="">
+                                <img src="{{ asset($item->product_thumbnail) }}" width="100" alt="">
                             </a>
                         </div>
                         <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
+                            <h6 class="mb-0 fw-light mb-1">{{ $item->product_name }}</h6>
                             <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                             </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
+                            @php
+                            $amount = $item->selling_price - $item->discount_price;
+                            $discount = round(($amount/$item->selling_price) * 100);
+                            @endphp
+                            <p class="mb-0 text-white">
+                                <!-- <strong>$59.00</strong> -->
+                                @if ($item->discount_price)
+                                <span class="me-1 text-decoration-line-through">${{ $item->selling_price }}</span>
+                                <span class="text-white">${{ $item->discount_price }}</span>
+                                @else
+                                <span class="text-white">${{ $item->selling_price }}</span>
+                                @endif
                             </p>
                         </div>
                     </div>
+                    @if ($key != count($recently_added) - 1)
                     <hr />
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/11.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/12.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
+                    @endif
+                    @endforeach
                 </div>
             </div>
             <div class="col">
                 <div class="top-rated-products-list mb-3">
-                    <h6 class="mb-3 text-uppercase">Top rated Products</h6>
+                    <h6 class="mb-3 text-uppercase">Special Deals</h6>
+                    @foreach ($special_deals as $key => $item)
                     <div class="d-flex align-items-center">
                         <div class="bottom-product-img">
                             <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/13.png') }}" width="100" alt="">
+                                <img src="{{ asset($item->product_thumbnail) }}" width="100" alt="">
                             </a>
                         </div>
                         <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
+                            <h6 class="mb-0 fw-light mb-1">{{ $item->product_name }}</h6>
                             <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                                 <i class="bx bxs-star text-white"></i>
                             </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
+                            @php
+                            $amount = $item->selling_price - $item->discount_price;
+                            $discount = round(($amount/$item->selling_price) * 100);
+                            @endphp
+                            <p class="mb-0 text-white">
+                                <!-- <strong>$59.00</strong> -->
+                                @if ($item->discount_price)
+                                <span class="me-1 text-decoration-line-through">${{ $item->selling_price }}</span>
+                                <span class="text-white">${{ $item->discount_price }}</span>
+                                @else
+                                <span class="text-white">${{ $item->selling_price }}</span>
+                                @endif
                             </p>
                         </div>
                     </div>
+                    @if ($key != count($special_deals) - 1)
                     <hr />
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/14.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/15.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div class="d-flex align-items-center">
-                        <div class="bottom-product-img">
-                            <a href="product-details.html">
-                                <img src="{{ asset('frontend/assets/images/products/16.png') }}" width="100" alt="">
-                            </a>
-                        </div>
-                        <div class="ms-0">
-                            <h6 class="mb-0 fw-light mb-1">Product Short Name</h6>
-                            <div class="rating font-12"> <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                                <i class="bx bxs-star text-white"></i>
-                            </div>
-                            <p class="mb-0 text-white"><strong>$59.00</strong>
-                            </p>
-                        </div>
-                    </div>
+                    @endif
+                    @endforeach
+
                 </div>
             </div>
         </div>

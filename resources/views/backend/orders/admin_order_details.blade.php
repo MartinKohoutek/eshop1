495f7b9561e3b0a72075c3c8f33ebafe32a20bf0
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-09-04 12:28:32 +0200

    Admin Order Details 2

--- resources/views/backend/orders/admin_order_details.blade.php
+++ resources/views/backend/orders/admin_order_details.blade.php
@@ -25,57 +25,176 @@
     <div class="row row-cols-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2">
         <div class="col">
             <div class="card">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="assets/images/gallery/10.png" alt="..." class="card-img">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>
-                            </p>
-                        </div>
-                    </div>
+                <div class="card-header">
+                    <h4>Shipping Details</h4>
+                </div>
+                <!-- <hr> -->
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>Shipping Name:</th>
+                            <td>{{ $order->name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Shipping Phone:</th>
+                            <td>{{ $order->phone }}</td>
+                        </tr>
+                        <tr>
+                            <th>Shipping Email:</th>
+                            <td>{{ $order->email }}</td>
+                        </tr>
+                        <tr>
+                            <th>Shipping Address:</th>
+                            <td>{{ $order->address }}</td>
+                        </tr>
+                        <tr>
+                            <th>Division:</th>
+                            <td>{{ $order['division']['division_name'] }}</td>
+                        </tr>
+                        <tr>
+                            <th>District:</th>
+                            <td>{{ $order['district']['district_name'] }}</td>
+                        </tr>
+                        <tr>
+                            <th>State:</th>
+                            <td>{{ $order['state']['state_name'] }}</td>
+                        </tr>
+                        <tr>
+                            <th>Post Code:</th>
+                            <td>{{ $order->post_code }}</td>
+                        </tr>
+                        <tr>
+                            <th>Order Date:</th>
+                            <td>{{ $order->order_date }}</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="assets/images/gallery/11.png" alt="..." class="card-img">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>
-                            </p>
-                        </div>
-                    </div>
+                <div class="card-header">
+                    <h4>Order Details</h4>
+                    <span class="text-warning">Invoice: {{ $order->invoice_no }}</span>
+                </div>
+                <!-- <hr> -->
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>Name:</th>
+                            <td>{{ $order['user']['name'] }}</td>
+                        </tr>
+                        <tr>
+                            <th>Email:</th>
+                            <td>{{ $order['user']['email'] }}</td>
+                        </tr>
+                        <tr>
+                            <th>Payment Type:</th>
+                            <td>{{ $order->payment_method }}</td>
+                        </tr>
+                        <tr>
+                            <th>Transaction ID:</th>
+                            <td>{{ $order->transaction_id }}</td>
+                        </tr>
+                        <tr>
+                            <th>Invoice:</th>
+                            <td>{{ $order->invoice_no }}</td>
+                        </tr>
+                        <tr>
+                            <th>Total Amount:</th>
+                            <td>${{ $order->amount }}</td>
+                        </tr>
+                        <tr>
+                            <th>Order Status:</th>
+                            <td>
+                                @if ($order->status == 'pending')
+                                <div class="badge rounded-pill bg-warning">Pending</div>
+                                @elseif ($order->status == 'confirm')
+                                <div class="badge rounded-pill bg-info">Confirm</div>
+                                @elseif ($order->status == 'processing')
+                                <div class="badge rounded-pill bg-danger">Processing</div>
+                                @elseif ($order->status == 'delivered')
+                                <div class="badge rounded-pill bg-success">Delivered</div>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td><a href="" class="btn btn-block btn-success">Confirm Order</a></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
     <div class="row row-cols-1 row-cols-md-1 row-cols-lg-1 row-cols-xl-1">
-					<div class="col">
-						<div class="card">
-							<div class="row g-0">
-								<div class="col-md-4">
-									<img src="assets/images/gallery/10.png" alt="..." class="card-img">
-								</div>
-								<div class="col-md-8">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-										<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+            <div class="card shadow-none mb-0">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="table-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Image</th>
+                                    <th>Product Name</th>
+                                    <th>Vendor Name</th>
+                                    <th>Product Code</th>
+                                    <th>Color</th>
+                                    <th>Size</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($orderItems as $key => $item)
+                                <tr>
+                                    <td>{{ $key+1 }}</td>
+                                    <td>
+                                        <img src="{{ asset($item['product']['product_thumbnail']) }}" alt="" style="width: 50px; height: 50px">
+                                    </td>
+                                    <td>{{ $item['product']['product_name'] }}</td>
+                                    <td>
+                                        @if ($item->vendor_id == NULL)
+                                        Owner
+                                        @else
+                                        {{ $item['product']['vendor']['name'] }}
+                                        @endif
+                                    </td>
+                                    <td>{{ $item['product']['product_code'] }}</td>
+                                    <td>
+                                        @if ($item['color'] == NULL)
+                                        ...
+                                        @else
+                                        {{ $item['color'] }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($item['size'] == NULL)
+                                        ...
+                                        @else
+                                        {{ $item['size'] }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($item['qty'] == NULL)
+                                        ...
+                                        @else
+                                        {{ $item['qty'] }}
+                                        @endif
+                                    </td>
+                                    <td>${{ $item['price'] }}
+                                        <br>Total: ${{$item['price'] * $item['qty'] }}
+                                    </td>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection

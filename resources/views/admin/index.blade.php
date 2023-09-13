32ca26260d9870592bb26b894642240da3762a72
Martin Kohoutek <martinkohoutek@hotmail.com>
2023-09-13 20:06:31 +0200

    Admin Dashbord - Display Pending Orders

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,16 +2,16 @@
 @section('admin')
 <div class="page-content">
     @php
-        $date = date('d-m-y');
-        $today_sale = App\Models\Order::where('order_date', $date)->sum('amount');
-        $month = date('F');
-        $month_sale = App\Models\Order::where('order_month', $month)->sum('amount');
-        $year = date('Y');
-        $year_sale = App\Models\Order::where('order_year', $year)->sum('amount');
+    $date = date('d-m-y');
+    $today_sale = App\Models\Order::where('order_date', $date)->sum('amount');
+    $month = date('F');
+    $month_sale = App\Models\Order::where('order_month', $month)->sum('amount');
+    $year = date('Y');
+    $year_sale = App\Models\Order::where('order_year', $year)->sum('amount');
 
-        $pending =App\Models\Order::where('status', 'pending')->get();
-        $vendor = App\Models\User::where('status', 'active')->where('role', 'vendor')->get();
-        $customer = App\Models\User::where('status', 'active')->where('role', 'user')->get();
+    $pending =App\Models\Order::where('status', 'pending')->get();
+    $vendor = App\Models\User::where('status', 'active')->where('role', 'vendor')->get();
+    $customer = App\Models\User::where('status', 'active')->where('role', 'user')->get();
     @endphp
     <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
         <div class="col">
@@ -110,7 +110,7 @@
         <div class="card-header">
             <div class="d-flex align-items-center">
                 <div>
-                    <h6 class="mb-0">Recent Orders</h6>
+                    <h6 class="mb-0">Pending Orders</h6>
                 </div>
                 <div class="dropdown ms-auto">
                     <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
@@ -129,106 +129,42 @@
                 </div>
             </div>
         </div>
+        @php
+        $orders = App\Models\Order::where('status', 'pending')->orderBy('id', 'DESC')->limit(10)->get();
+        @endphp
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table align-middle mb-0">
                     <thead class="table-light">
                         <tr>
-                            <th>Product</th>
-                            <th>Photo</th>
-                            <th>Product ID</th>
-                            <th>Status</th>
-                            <th>Amount</th>
+                            <th>#</th>
                             <th>Date</th>
-                            <th>Shipping</th>
+                            <th>Invoice</th>
+                            <th>Amount</th>
+                            <th>Payment</th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($orders as $key => $order)
                         <tr>
-                            <td>Iphone 5</td>
-                            <td><img src="{{ asset('backend/assets/images/products/01.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#9405822</td>
-                            <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-                            <td>$1250.00</td>
-                            <td>03 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-                                </div>
-                            </td>
+                            <td>{{ $key+1 }}</td>
+                            <td>{{ $order->order_date }}</td>
+                            <td><a href="{{ route('admin.order.details', $order->id) }}">{{ $order->invoice_no }}</a></td>
+                            <td>${{ $order->amount }}</td>
+                            <td>{{ $order->payment_method }}</td>
+                            <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">{{ $order->status }}</span></td>
                         </tr>
-
-                        <tr>
-                            <td>Earphone GL</td>
-                            <td><img src="{{ asset('backend/assets/images/products/02.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#8304620</td>
-                            <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-                            <td>$1500.00</td>
-                            <td>05 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-                                </div>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>HD Hand Camera</td>
-                            <td><img src="{{ asset('backend/assets/images/products/03.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#4736890</td>
-                            <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-                            <td>$1400.00</td>
-                            <td>06 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 70%"></div>
-                                </div>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Clasic Shoes</td>
-                            <td><img src="{{ asset('backend/assets/images/products/04.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#8543765</td>
-                            <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-                            <td>$1200.00</td>
-                            <td>14 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Sitting Chair</td>
-                            <td><img src="{{ asset('backend/assets/images/products/06.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#9629240</td>
-                            <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-                            <td>$1500.00</td>
-                            <td>18 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Hand Watch</td>
-                            <td><img src="{{ asset('backend/assets/images/products/05.png') }}" class="product-img-2" alt="product img"></td>
-                            <td>#8506790</td>
-                            <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-                            <td>$1800.00</td>
-                            <td>21 Feb 2020</td>
-                            <td>
-                                <div class="progress" style="height: 6px;">
-                                    <div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 40%"></div>
-                                </div>
-                            </td>
-                        </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
+<style>
+    td a:hover {
+        color: red;
+    }
+</style>
 @endsection
